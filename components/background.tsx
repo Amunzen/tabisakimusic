18400a1ede249dbb9f3593fcaca146e711ffc0f0
Keisuke Nagakawa <kay@amunzen.com>
2024-05-16 13:37:29 +0900

    背景画像を追加し、全体に適用されるようにスタイルを更新します。

---
+++ components/background.tsx
@@ -0,0 +1,22 @@
+const imageUrl = `/bg.png`
+
+export function Background() {
+  return <div style={styles.backgroundMain} />
+}
+
+const styles: { [key: string]: React.CSSProperties } = {
+  backgroundMain: {
+    width: '100vw',
+    minHeight: '100vh',
+    position: 'fixed',
+    zIndex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '120px 24px 160px 24px',
+    pointerEvents: 'none',
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: 'cover', // Ensures the background image covers the entire area
+    backgroundRepeat: 'no-repeat', // Prevents the background image from repeating
+    backgroundPosition: 'center' // Centers the background image
+  }
+}
