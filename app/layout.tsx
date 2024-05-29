5f54f5bf141444e397c15348295be210f67405c7
Keisuke Nagakawa <kay@amunzen.com>
2024-05-30 06:09:45 +0900

    不要なフォントインポートとクラス名の使用を削除する。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 
 import '@/app/globals.css'
-import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
@@ -39,13 +36,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'font-sans antialiased',
-          GeistSans.variable,
-          GeistMono.variable
-        )}
-      >
+      <body>
         <Background />
         <Toaster position="top-center" />
         <Providers
