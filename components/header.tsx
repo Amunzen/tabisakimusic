0604871b6b44aaf6bd166454cb68205e4828e9a4
Keisuke Nagakawa <kay@amunzen.com>
2024-05-30 06:13:15 +0900

    ヘッダーに画像ロゴを追加し、レイアウトを調整します。

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,17 @@
+import Image from 'next/image'
+
 function Logo() {
   return (
-    <span className="font-extrabold text-white text-xl ">旅先コンシェルジュ 楽曲生成モードβ版</span>
+    <span className="font-extrabold text-white text-xl ">
+      旅先コンシェルジュ 楽曲生成モードβ版
+    </span>
   )
 }
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 flex w-full h-16 px-4 shrink-0 items-center justify-center">
-      <Logo />
+    <header className="sticky top-0 z-50 flex w-full px-4 shrink-0 items-center justify-center">
+      <Image src="/pc_noren.png" alt="logo" width={1000} height={300} />
     </header>
   )
 }
