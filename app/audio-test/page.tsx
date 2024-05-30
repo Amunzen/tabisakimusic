d37b6b21993539b692b66146bb76fe7a6c59010a
Keisuke Nagakawa <kay@amunzen.com>
2024-05-30 09:34:25 +0900

    APIエンドポイントをローカルホストからデプロイされたURLに変更します。

--- app/audio-test/page.tsx
+++ app/audio-test/page.tsx
@@ -3,7 +3,7 @@ import { Content } from './content'
 import SongComponent from '@/components/music/song2'
 export default async function Page() {
   const url =
-    'http://localhost:3000/api/get?ids=357c8cb0-28ac-4a75-b52a-c886489bd823'
+    'https://suno-api-opal-alpha.vercel.app/api/get?ids=357c8cb0-28ac-4a75-b52a-c886489bd823'
 
   const res = await fetch(url)
   const data = await res.json()
